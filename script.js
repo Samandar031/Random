67083a6c10e2042f17c6f34c20e2141d829b57ef
Samandar Qodirqulov <samandarqodirqulov021@gmail.com>
2022-03-29 23:33:10 +0500

    js tugadi hali kamchiligi mavjud

---
+++ script.js
@@ -0,0 +1,23 @@
+let one = document.querySelector(".number_one");
+let two = document.querySelector(".number_two");
+
+let input = document.querySelector(".number_input");
+let btn = document.querySelector(".btn");
+
+let a = Math.trunc(Math.random() * 100 + 1);
+let b = Math.trunc(Math.random() * 100 + 1);
+console.log(a);
+console.log(b);
+
+one.textContent = a;
+two.textContent = b;
+
+btn.addEventListener("click", function () {
+  if (a + b == input.value) {
+    btn.style.backgroundColor = "green";
+    alert("to'g'ri topdingiz");
+  } else {
+    btn.style.backgroundColor = "red";
+    alert("javobingiz noto'g'ri");
+  }
+});
